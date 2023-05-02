c5c21cfbc2119da38dcad7da1643e3adc996093c
Tomasz Lisowski <t.p.lisowski@gmail.com>
2023-05-02 23:55:52 +0200

    In example, add error reason for scraw calls and add null to reader name

--- example/src/main.c
+++ example/src/main.c
@@ -5,15 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int32_t main()
+int32_t main(void)
 {
     int32_t main_ret = EXIT_FAILURE;
     int32_t ret;
     scraw_st ctx;
     char *reader_name_selected = NULL;
-    if (scraw_init(&ctx) == 0)
+    int const err_init = scraw_init(&ctx);
+    if (err_init == 0)
     {
-        if (scraw_reader_search_begin(&ctx) == 0)
+        int const err_reader_search_begin = scraw_reader_search_begin(&ctx);
+        if (err_reader_search_begin == 0)
         {
             bool reader_found = false;
             char const *reader_name_tmp = NULL;
@@ -27,7 +29,7 @@ int32_t main()
                     {
                         printf("Selecting reader %u\n", reader_idx);
                         uint64_t const reader_name_len =
-                            strlen(reader_name_tmp);
+                            strlen(reader_name_tmp) + 1 /* null-terminator */;
                         reader_name_selected = malloc(reader_name_len);
                         if (reader_name_selected == NULL)
                         {
@@ -41,7 +43,8 @@ int32_t main()
                             if (scraw_reader_select(&ctx,
                                                     reader_name_selected) == 0)
                             {
-                                printf("Selected reader %u\n", reader_idx);
+                                printf("Selected reader %u \"%s\"\n",
+                                       reader_idx, ctx.reader_selected.name);
                                 reader_found = true;
                                 break; /* Selected a reader so can carry on. */
                             }
@@ -50,18 +53,25 @@ int32_t main()
                 }
                 else if (ret == 1)
                 {
-                    printf("Reached end of reader list\n");
+                    printf("Reached end of reader list "
+                           "scraw_reader_search_next()=%i reason=0x%llx\n",
+                           ret, ctx.err_reason);
                     break;
                 }
                 else
                 {
-                    printf("Error while searching reader list\n");
+                    printf("Error while searching reader list "
+                           "scraw_reader_search_next()=%i reason=0x%llx\n",
+                           ret, ctx.err_reason);
                     break;
                 }
             }
-            if (scraw_reader_search_end(&ctx) != 0)
+            int const err_reader_search_end = scraw_reader_search_end(&ctx);
+            if (err_reader_search_end != 0)
             {
-                printf("Failed to end search\n");
+                printf("Failed to end search scraw_reader_search_end()=%i  "
+                       "reason=0x%llx\n",
+                       err_reader_search_end, ctx.err_reason);
                 /**
                  * This failure leads to a memory leak but is not critical so we
                  * carry on.
@@ -69,9 +79,12 @@ int32_t main()
             }
             if (reader_found)
             {
-                if (scraw_card_connect(&ctx, SCRAW_PROTO_T0) == 0)
+                int const err_connect =
+                    scraw_card_connect(&ctx, SCRAW_PROTO_T0);
+                if (err_connect == 0)
                 {
-                    printf("Connected to card\n");
+                    printf("Connected to card scraw_card_connect()=%i\n",
+                           err_connect);
                     uint8_t const raw_data[] = {0xA0, 0xA4, 0x00, 0x00,
                                                 0x02, 0x3F, 0x00};
                     scraw_raw_st raw = {
@@ -84,9 +97,10 @@ int32_t main()
                         .buf_len = sizeof(res_data) / sizeof(res_data[0U]),
                         .res_len = 0,
                     };
-                    if (scraw_send(&ctx, &raw, &res) == 0)
+                    int const err_send = scraw_send(&ctx, &raw, &res);
+                    if (err_send == 0)
                     {
-                        printf("Sent data to card\n");
+                        printf("Sent data to card scraw_send()=%i\n", err_send);
                         printf("Response: ");
                         for (uint32_t res_idx = 0U; res_idx < res.res_len;
                              ++res_idx)
@@ -98,36 +112,52 @@ int32_t main()
                     }
                     else
                     {
-                        printf("Failed to send data to card\n");
+                        printf("Failed to send data to card scraw_send()=%i "
+                               "reason=0x%llx\n",
+                               err_send, ctx.err_reason);
                     }
 
-                    if (scraw_card_disconnect(&ctx) == 0)
+                    int const err_disconnect = scraw_card_disconnect(&ctx);
+                    if (err_disconnect == 0)
                     {
-                        printf("Disconnected from card\n");
+                        printf("Disconnected from card "
+                               "scraw_card_disconnect()=%i\n",
+                               err_disconnect);
                     }
                     else
                     {
-                        printf("Failed to disconnect from card\n");
+                        printf("Failed to disconnect from card "
+                               "scraw_card_disconnect()=%i reason=0x%llx\n",
+                               err_disconnect, ctx.err_reason);
                     }
                 }
                 else
                 {
-                    printf("Failed to connect to card\n");
+                    printf("Failed to connect to card scraw_card_connect()=%i "
+                           "reason=0x%llx\n",
+                           err_connect, ctx.err_reason);
                 }
             }
         }
         else
         {
-            printf("Failed to search for readers\n");
+            printf("Failed to search for readers "
+                   "scraw_reader_search_begin()=%i reason=0x%llx\n",
+                   err_reader_search_begin, ctx.err_reason);
         }
-        if (scraw_fini(&ctx) != 0)
+        int const err_fini = scraw_fini(&ctx);
+        if (err_fini != 0)
         {
-            printf("Failed to deinitialize the library\n");
+            printf("Failed to deinitialize the library scraw_fini()=%i "
+                   "reason=0x%llx\n",
+                   err_fini, ctx.err_reason);
         }
     }
     else
     {
-        printf("Failed to initialize the library\n");
+        printf(
+            "Failed to initialize the library scraw_init()=%i reason=0x%llx\n",
+            err_init, ctx.err_reason);
     }
     if (reader_name_selected != NULL)
     {
